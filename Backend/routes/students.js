b673a315edfda982679553dcdeb5244b8fdb1095
Faariis <ftrtak@gmail.com>
2024-01-23 17:16:40 +0100

    Adding same checks to the UPDATE method that I added to the POST method. Only difference is that I eliminated the fake positives by including "AND StudentID <> ?".

--- Backend/routes/students.js
+++ Backend/routes/students.js
@@ -24,7 +24,7 @@ router.post("/add", (req, res) => {
   if (students.Semester <= 0 || students.Semester >= 12) {
     return res.status(400).json({ error: "Ima 12 semestara [1-12]!" });
   }
-
+  // Provjera enumeracije, da mora biti student
   if (students.UserType !== "student") {
     return res.status(400).json({ error: "User type mora biti student!" });
   }
@@ -214,6 +214,155 @@ router.post("/add", (req, res) => {
 router.patch("/update", (req, res) => {
   let students = req.body;
 
+  // Provjera jer ima 12 semestara i ne smije biti slovo
+  if (students.Semester <= 0 || students.Semester >= 12) {
+    return res.status(400).json({ error: "Ima 12 semestara [1-12]!" });
+  }
+  // Provjera enumeracije, da mora biti student
+  if (students.UserType !== "student") {
+    return res.status(400).json({ error: "User type mora biti student!" });
+  }
+
+  // Provjerava da li postoji student sa istim brojem indeksa
+  const checkStudentIndexNumber =
+    "SELECT * FROM students WHERE IndexNumber = ? AND StudentID <> ?";
+  connection.query(
+    checkStudentIndexNumber,
+    [students.IndexNumber, students.StudentID],
+    (err, studentIN) => {
+      if (err) {
+        console.error("Error checking student index number existence:", err);
+        return res
+          .status(500)
+          .json({ error: "Error checking student index number existence" });
+      }
+
+      if (studentIN.length !== 0) {
+        return res
+          .status(400)
+          .json({ error: "Već postoji student sa tim indeksom." });
+      } else if (students.IndexNumber <= 0 || students.IndexNumber >= 1000) {
+        return res.status(400).json({
+          error:
+            "Vrijednost indeksa ne smije biti manje od 1 ili veće od 1000!",
+        });
+      } else if (!/^\d+$/.test(students.IndexNumber)) {
+        return res
+          .status(400)
+          .json({ message: "Indeks mora se sastojati od brojeva!" });
+      }
+
+      // Provjerava da li postoji student sa istim matičnim brojem
+      const checkStudentJMBG = "SELECT * FROM students WHERE JMBG = ? AND StudentID <> ?";
+      connection.query(
+        checkStudentJMBG,
+        [students.JMBG, students.StudentID],
+        (err, studentJMBG) => {
+          if (err) {
+            console.error("Error checking student JMBG existence:", err);
+            return res
+              .status(500)
+              .json({ error: "Error checking student JMBG existence" });
+          }
+
+          if (studentJMBG.length !== 0) {
+            return res
+              .status(400)
+              .json({ error: "Već postoji student sa tim matičnim brojem." });
+          } else if (students.JMBG.length !== 13) {
+            return res.status(400).json({
+              error:
+                "Vrijednost matičnog broja ne smije biti manja ili veća od 13!",
+            });
+          } else if (!/^\d+$/.test(students.JMBG)) {
+            return res
+              .status(400)
+              .json({ message: "JMBG mora se sastojati od brojeva!" });
+          }
+
+          // Provjerava postoje li dva studenta sa istim imenom i prezimenom
+          const checkStudentFirstNameLastName =
+            "SELECT * FROM students WHERE FirstName = ? AND LastName = ? AND StudentID <> ?";
+          connection.query(
+            checkStudentFirstNameLastName,
+            [students.FirstName, students.LastName, students.StudentID],
+            (err, studentFL) => {
+              if (err) {
+                console.error(
+                  "Error checking student first and last name existence:",
+                  err
+                );
+                return res.status(500).json({
+                  error: "Error checking student first and last name existence",
+                });
+              }
+
+              if (studentFL.length !== 0) {
+                return res.status(400).json({
+                  error: "Već postoji student sa tim imenom i prezimenom.",
+                });
+              } else if (/\d/.test(students.LastName)) {
+                return res
+                  .status(400)
+                  .json({ message: "Prezime ne smije sadržavati brojeve!" });
+              } else if (/\d/.test(students.FirstName)) {
+                return res
+                  .status(400)
+                  .json({ message: "Ime ne smije sadržavati brojeve!" });
+              }
+
+              // Provjerava da li postoji student sa istim mailom
+              const checkStudentEmail =
+                "SELECT * FROM students WHERE Email = ? AND StudentID <> ?";
+              connection.query(
+                checkStudentEmail,
+                [students.Email, students.StudentID],
+                (err, studentEmail) => {
+                  if (err) {
+                    console.error(
+                      "Error checking student email existence:",
+                      err
+                    );
+                    return res.status(500).json({
+                      error: "Error checking student email existence",
+                    });
+                  }
+
+                  if (studentEmail.length !== 0) {
+                    return res
+                      .status(400)
+                      .json({ error: "Već postoji student sa tim mailom." });
+                  }
+
+
+               // Provjerava da li postoji student sa istim brojem telefona
+               const checkStudentPhone =
+               "SELECT * FROM students WHERE Phone = ? AND StudentID <> ?";
+             connection.query(
+               checkStudentPhone,
+               [students.Phone, students.StudentID],
+               (err, studentPhone) => {
+                 if (err) {
+                   console.error(
+                     "Error checking student phone existence:",
+                     err
+                   );
+                   return res.status(500).json({
+                     error: "Error checking student phone existence",
+                   });
+                 }
+
+                 if (studentPhone.length !== 0) {
+                   return res.status(400).json({
+                     error: "Već postoji student sa tim brojem telefona.",
+                   });
+                 } else if (!/^\d+$/.test(students.Phone)) {
+                   return res.status(400).json({
+                     message: "Telefon mora se sastojati od brojeva!",
+                   });
+                 }
+
+// UPDATE Glavna funkcija
   var query =
     "update students set RegistrationYear=?, Semester=?, IndexNumber=?, JMBG=?, FirstName=?, LastName=?, Gender=? ,Email=?, Phone=?, DateOfBirth=?, PlaceOfBirth=?, Address=?, UserType=?, Password=? where StudentID=?";
   connection.query(
@@ -233,23 +382,24 @@ router.patch("/update", (req, res) => {
       students.Address,
       students.UserType,
       students.Password,
-      students.StudentID,
+      students.StudentID
     ],
     (err, results) => {
       if (!err) {
-        if (results.affectedRows == 0) {
-          return res
-            .status(400)
-            .json({ message: "Id biranog studenta ne postoji!" });
+        if (results.affectedRows === 0) {
+          return res.status(404).json({ message: "Id biranog studenta ne postoji!" });
         }
-        return res
-          .status(200)
-          .json({ message: "Student uspješno izmijenjen!" });
+        return res.status(200).json({ message: "Student uspješno izmijenjen!" });
       } else {
         return res.status(500).json(err);
       }
     }
   );
+});
+});
+});
+});
+});
 });
 
 module.exports = router;
