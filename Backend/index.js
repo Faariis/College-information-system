f3afc7084bc356cd6e9090c603e28c31e5065b3a
Faariis <ftrtak@gmail.com>
2024-01-25 18:27:45 +0100

    Added new route login for testing purpouses.

--- Backend/index.js
+++ Backend/index.js
@@ -4,12 +4,14 @@ const connection = require('./connection');
 const app = express();
 const studentsRoute = require('./routes/students');
 const adminsRoute = require('./routes/admins');
+const loginRoute = require('./routes/login');
 
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use('/students', studentsRoute);
 app.use('/admins', adminsRoute);
+app.use('/login', loginRoute);
 
 module.exports = app;
 
---
+++ Backend/routes/login.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const connection = require("../connection");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+
+// LOGIN
+
+
+
+
+module.exports = router;
