b4da9bc67763c5aae69d7b0e8c32e743e99ce0bc
Faariis <ftrtak@gmail.com>
2024-01-25 19:06:09 +0100

    Added courses route.

--- Backend/index.js
+++ Backend/index.js
@@ -5,6 +5,7 @@ const app = express();
 const studentsRoute = require('./routes/students');
 const adminsRoute = require('./routes/admins');
 const loginRoute = require('./routes/login');
+const coursesRoute = require('./routes/courses');
 
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use('/students', studentsRoute);
 app.use('/admins', adminsRoute);
 app.use('/login', loginRoute);
+app.use('/courses', coursesRoute);
 
 module.exports = app;
 
