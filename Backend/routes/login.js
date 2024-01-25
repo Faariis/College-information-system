e633c905d708b00f75913bd356357b90d068b4bc
Faariis <ftrtak@gmail.com>
2024-01-25 18:52:02 +0100

    Added new login method to the route login for testing. It works.

--- Backend/routes/login.js
+++ Backend/routes/login.js
@@ -6,6 +6,43 @@ const jwt = require("jsonwebtoken");
 
 
 // LOGIN
+router.post('/login', (req, res) =>{
+    const { Email, Password, UserType } = req.body;
+
+    // Biranje tabele na osnovu korisnika
+    const tableName = UserType === 'admin' ? 'admins' : 'students';
+
+    const query = `SELECT * FROM ${tableName} WHERE Email = ?`;
+
+    connection.query(query, [Email], async(error, results) =>{
+        if (error) {
+            console.error("Error in login:", error);
+            return res.status(500).json(error);
+        }
+        if (results.length === 0) {
+             return res.status(200).json({message: "Nema korisnika!"});
+        }
+
+        const user = results[0];
+
+        try {
+
+            // Poređenje šifri
+            const passwordMatch = await bcrypt.compare(Password, user.Password);
+            
+            if (passwordMatch) {
+            // JWT
+            const token = jwt.sign({ UserID : user.UserID, UserType: user.UserType }, 'your-secret-key', { expiresIn: '1h' });
+            return res.status(200).json({message: "Uspješna prijava!", token, UserType: user.UserType});
+            } else {
+                return res.status(500).json({message: "Neuspješna prijava!"});
+            }
+        } catch (compareError) {
+            console.error("Error comparing password:", compareError);
+            return res.status(500).json({message: "Internal server error!"});
+        }
+    });
+});
 
 
 
