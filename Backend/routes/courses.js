785aa65bc4ecbaba4b38e78dc9920af47efe19d6
Faariis <ftrtak@gmail.com>
2024-01-28 23:16:29 +0100

    Added post method to courses without check.

--- Backend/routes/courses.js
+++ Backend/routes/courses.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connection = require("../connection");
+const { ConnectionAcquireTimeoutError } = require("sequelize");
 const router = express.Router();
 
 // GET courses
@@ -13,6 +14,21 @@ router.get('/get', (req, res) =>{
           {
             return res.status(400).json(err);
           }
+    });
+});
+
+// POST
+router.post('/post', (req, res) => {
+    let courses = req.body;
+
+    const query = 'INSERT INTO courses (CourseCode, CourseName, ECTSCredits) VALUES (?,?,?)';
+
+    connection.query(query, [courses.CourseCode, courses.CourseName, courses.ECTSCredits], (err, results) =>{
+        if (!err) {
+            return res.status(200).json({message: "Uspješno dodan predmet!"});
+        } else {
+            return res.status(400).json(err);
+        }
     });
 });
 
