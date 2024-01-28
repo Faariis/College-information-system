d381f56a47369f58a61eae9e13bb3fd1db9a3be5
Faariis <ftrtak@gmail.com>
2024-01-28 23:08:51 +0100

    Added get method to courses.

--- Backend/routes/courses.js
+++ Backend/routes/courses.js
@@ -3,6 +3,18 @@ const connection = require("../connection");
 const router = express.Router();
 
 // GET courses
+router.get('/get', (req, res) =>{
+    const query = 'SELECT * FROM courses ORDER BY CourseName';
+
+    connection.query(query, (err, results) =>{
+          if (!err) {
+            return res.status(200).json(results);
+          } else
+          {
+            return res.status(400).json(err);
+          }
+    });
+});
 
 
 module.exports = router;
